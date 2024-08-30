4aabcf364232ad3e951e5dfbc36f11c496e03a03
Wtsmen34 <arcefr34@gmail.com>
2024-08-30 16:38:13 -0300

    primer commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        int a=3;
+                int b=3;
+ int resultado= (a+b);
 
+                System.out.println(resultado);
+
+                String hola="hola mundo";
+                String holA="chau";
+
+                System.out.println(hola + holA);
+                
 
 
 
